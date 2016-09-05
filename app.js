62ec8fb8215cd7e0926c0137c7c7e241ef19a46a
Aakash Sigdel <aakash.sigdel@gmail.com>
2016-09-05 08:25:43 +0545

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+import React, { PropTypes, Component } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import FCM from 'react-native-fcm'
+
+class App extends Component {
+  componentDidMount() {
+    console.log('Open from Notif')
+    console.log(FCM.initialData)
+    console.log('=====================')
+
+    FCM.requestPermissions(); // for iOS
+    FCM.getFCMToken().then(token => {
+      console.log(token)
+      console.log('first')
+      // store fcm token in your server
+    });
+    this.notificationUnsubscribe = FCM.on('notification', (notif) => {
+      // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
+      console.log(notif)
+    });
+    this.refreshUnsubscribe = FCM.on('refreshToken', (token) => {
+      console.log(token)
+      console.log('second')
+      // fcm token may not be available on first load, catch it here
+    });
+
+    FCM.subscribeToTopic('/topics/foo-bar');
+    FCM.unsubscribeFromTopic('/topics/foo-bar');
+  }
+
+  componentWillUnmount() {
+    // prevent leaking
+    this.refreshUnsubscribe();
+    this.notificationUnsubscribe();
+  }
+
+  render() {
+    return (
+      <Text style={{marginTop: 80}}>Hello There</Text>
+    )
+  }
+}
+
+export default App
